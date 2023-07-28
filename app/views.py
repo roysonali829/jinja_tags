2e777d9d4f792bb991d8c56076ec9f0925507ce4
roysonali829 <roysonali829@gmail.com>
2023-07-28 12:00:30 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def data_render(request):
     d={'name':'bristi','age':23}
     return render(request,'data_render.html',context=d)
+
+
+def if_block(request):
+    d={'a':10}
+    return render(request,'if_block.html',context=d)
